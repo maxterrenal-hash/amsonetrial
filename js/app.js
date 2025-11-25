9eb10cbebe3e13c348e6347bbeeac2af04ad5863
maxterrenal-hash <maxterrenal@gmail.com>
2025-11-25 14:32:08 +0800

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// app.js
+import { buildDrugLists } from "./drugs.js";
+import { initModeToggle, applyModeMode } from "./mode.js";
+import { initEgfr } from "./egfr.js";
+import { initMonograph } from "./monograph.js";
+import { initDynamicBlocks } from "./dynamicRows.js";
+import { initValidation } from "./validation.js";
+import { initSubmit } from "./submit.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  buildDrugLists();
+  initModeToggle();
+  initEgfr();
+  initMonograph();
+  initDynamicBlocks();
+  initValidation();
+  initSubmit();
+
+  applyModeMode();
+});
